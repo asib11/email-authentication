439864963d724588b8c477bb9dfe4359deb8dff9
asib <ahmedasib1@gmail.com>
2023-10-31 11:42:19 +0600

    login with gmail

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,47 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../../firebase/firebase.init';
 
 const Login = () => {
+    const [loginError, setLoginError] = useState('');
+    const [success, SetSuccess] = useState('');
+    const [user, setUser] = useState(null);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        setLoginError('');
+        SetSuccess('');
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setLoginError('password must be one uppercase');
+            return;
+        }
+        //firebase connect
+        const auth = getAuth(app);
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setUser(user);
+                SetSuccess('login successfully');
+                form.reset();
+            })
+            .catch(error => {
+                setLoginError(error.message);
+            })
+    }
+
     return (
         <div>
             <h4>Please Login</h4>
-            <Form >
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" name='email' placeholder="Enter email" />
@@ -20,9 +56,15 @@ const Login = () => {
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Login
                 </Button>
             </Form>
+            {
+                user && <div>
+                    <p className='text-danger'>{loginError}</p>
+                    <p className='text-success'>{success}</p>
+                </div>
+            }
         </div>
     );
 };
